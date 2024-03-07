f00e33f8c0d9e545fd435881bda8bd906e0ecf51
Igor Ulian <igorulian@outlook.com>
2024-03-07 12:09:03 -0300

    feat: remove item from cart

--- src/pages/presentes.tsx
+++ src/pages/presentes.tsx
@@ -3,8 +3,8 @@ import GIFTS from "@/assets/database/gifts.json";
 
 export default function Presentes() {
   return (
-    <main className="flex items-center justify-center bg-blue-50">
-      <div className="justify-center w-full md:w-2/3 flex flex-row flex-wrap min-h-screen min-w-screen p-4 md:p-8 gap-2 md:gap-8">
+    <main className="flex items-center justify-center bg-white">
+      <div className="justify-around sm:justify-center w-full md:w-2/3 flex flex-row flex-wrap min-h-screen min-w-screen p-4 md:p-8 gap-2 md:gap-8">
         {GIFTS.map((p, index) => (
           <Product product={p} key={index} />
         ))}

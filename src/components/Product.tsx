571c33940a16930c48db815641072db2346c5bec
Igor Ulian <igorulian@outlook.com>
2024-03-07 12:08:35 -0300

    fix: Add response behavior

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
 
 const Produto = ({ product }: { product: { name: string; price: number } }) => {
-  
-  function addToCart(){
-    const localCart = localStorage.getItem('@cart')
-    
-    const cart = localCart ? JSON.parse(localCart) : []
-    cart.push(product)
-    localStorage.setItem('@cart', JSON.stringify(cart))
+  function addToCart() {
+    const localCart = localStorage.getItem("@cart");
 
-    window.location.href = 'carrinho'
+    const cart = localCart ? JSON.parse(localCart) : [];
+    cart.push(product);
+    localStorage.setItem("@cart", JSON.stringify(cart));
+
+    window.location.href = "carrinho";
   }
-  
+
   return (
-    <div className="flex justify-between flex-col items-center p-4 rounded-lg bg-white h-auto w-40 sm:w-56 ">
+    <div className="flex shadow-lg gap-2 justify-between flex-col items-center p-4 rounded-lg bg-white h-auto w-40 md:w-56">
       <Image
         alt="A"
         src={require("../assets/gifts/gift_1.jpg")}
         className="w-100 bg-black h-100 rounded-md"
       />
-      <span className="text-color-txt text-sm mt-2 text-center">
-        {product.name}
-      </span>
-      <span className="text-base mb-2 text-color-txt">R${product.price}</span>
-      <button onClick={addToCart} className="flex text-lg items-center justify-center w-full p-2 text-white bg-primary rounded-lg">
+      <div className="flex flex-col h-full justify-between items-center">
+        <span className="text-color-txt text-xs sm:text-sm text-center">
+          {product.name}
+        </span>
+        <span className="text-base text-gray-600">R${product.price}</span>
+      </div>
+      <button
+        onClick={addToCart}
+        className="flex text-regular items-center justify-center w-full p-2 text-white bg-primary rounded-lg"
+      >
         Presentear
       </button>
     </div>
